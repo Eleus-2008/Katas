1e12bbf59124e2f0813f4bff2b1fed9aa839219e
Anton Eliseev <anton2008.e@yandex.ru>
2021-07-14 00:15:04 +0400

    add numbers in expanded form kata

---
+++ Katas/NumbersInExpandedForm.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+
+namespace Katas
+{
+    public static class NumbersInExpandedForm
+    {
+        public static string ExpandedForm(long num)
+        {
+            var numString = num.ToString();
+            var sb = new StringBuilder();
+
+            for (var i = 0; i < numString.Length; i++)
+            {
+                var digit = (int) char.GetNumericValue(numString[i]);
+
+                if (digit != 0)
+                {
+                    if (i != 0)
+                    {
+                        sb.Append(" + ");
+                    }
+                    sb.Append(digit);
+                    sb.Append('0', numString.Length - i - 1);
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
