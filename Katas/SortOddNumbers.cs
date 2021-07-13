05c64461ab941b11856bd5f4ab5b623c92715934
Anton Eliseev <anton2008.e@yandex.ru>
2021-07-13 23:36:26 +0400

    add sorting odd numbers kata

---
+++ Katas/SortOddNumbers.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+
+namespace Katas
+{
+    public static class SortOddNumbers
+    {
+        public static int[] SortArray(int[] array)
+        {
+            var oddNumbersWithIndexes = array.Select((n, i) => (n, i)).Where(n => n.n % 2 == 1).ToArray();
+            var oddNumbers = oddNumbersWithIndexes.Select(ni => ni.n).ToList();
+            var indexes = oddNumbersWithIndexes.Select(ni => ni.i).ToArray();
+            oddNumbers.Sort();
+
+            for (var i = 0; i < oddNumbers.Count; i++)
+            {
+                array[indexes[i]] = oddNumbers[i];
+            }
+
+            return array;
+        }
+    }
+}
