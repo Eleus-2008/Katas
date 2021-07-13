6743aa8cd5e507b10be3776ad49c0b056fb01ed9
Anton Eliseev <anton2008.e@yandex.ru>
2021-07-13 23:03:09 +0400

    add finding loop length in linked list kata

---
+++ Katas/LinkedListLoop.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace Katas
+{
+    public abstract class LoopDetector
+    {
+        public abstract class Node
+        {
+            public abstract Node Next();
+        }
+    }
+
+
+    public static class LinkedListLoop
+    {
+        public static int GetLoopSize(LoopDetector.Node startNode)
+        {
+            var nodes = new Dictionary<LoopDetector.Node, int> {{startNode, 0}};
+            var currentNode = startNode;
+            var i = 0;
+
+            while (true)
+            {
+                i++;
+                currentNode = currentNode.Next();
+
+                if (!nodes.TryAdd(currentNode, i))
+                {
+                    var result = nodes.Count - nodes[currentNode];
+                    return result;
+                }
+            }
+        }
+    }
+}
